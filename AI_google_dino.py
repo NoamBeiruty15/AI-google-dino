8ed1c88bd3c7f3b2d3f73cda6c27d1538d1ea4a7
Noam beiruty <noambeiruty@noams-mbp.home>
2024-10-23 13:02:05 +0300

    AI is working

--- AI_google_dino.py
+++ AI_google_dino.py
@@ -2,12 +2,13 @@ import pygame
 import sys
 import os
 import random
+import neat
 
 # Initialize game
 pygame.init()
 
-SCREEN_WIDTH = 700
-SCREEN_HEIGHT = 400
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 550
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Google Dino")
 clock = pygame.time.Clock()
@@ -16,7 +17,8 @@ clock = pygame.time.Clock()
 FPS = 60
 GRAVITY = 1
 JUMP_STRENGTH = -16
-GROUND_LEVEL = 235
+GROUND_LEVEL = 400
+generation = 0
 
 # Load assets
 DINO_RUNNING_IMAGES = [
@@ -127,7 +129,7 @@ class Bird:
         self.alive = True
         self.passed = False
 
-    def update(self, speed):
+    def update(self, speed, genomes):
         self.rect.x -= speed
         
         self.animation_frame = (self.animation_frame + 0.1) % len(self.images)
@@ -135,21 +137,26 @@ class Bird:
 
         if not self.passed and self.rect.right < 50:
             self.passed = True
+            for i, genome in enumerate(genomes):
+                genomes[i].fitness += 1
 
-        if self.rect.right < 0:
+        if self.rect.x < 0:
             self.alive = False
         
 
     def draw(self, surface):
         surface.blit(self.image, self.rect)
 
-    def check_colliding(self, dinos):
-        for dino in dinos:
+    def check_colliding(self, dinos, genomes, networks):
+        for i, dino in enumerate(dinos):
             if self.rect.colliderect(dino.rect):
                 offset_x = self.rect.x - dino.rect.x
                 offset_y = self.rect.y - dino.rect.y
 
                 if dino.mask.overlap(pygame.mask.from_surface(self.image), (offset_x, offset_y)):
+                    genomes[i].fitness -= 10
+                    genomes.pop(i)
+                    networks.pop(i)
                     dinos.remove(dino)  
 
 class Cactus:
@@ -196,55 +203,90 @@ class Cactus:
         self.passed = False
         self.alive = True
 
-    def update(self, speed):
+    def update(self, speed, genomes):
         self.rect.x -= speed 
 
-        if not self.passed and self.rect.right < 50:
+        if self.passed == False and self.rect.x < 50:
             self.passed = True
+            for i, genome in enumerate(genomes):
+                genomes[i].fitness += 1
 
-        if self.rect.right < 0: 
+        if self.rect.x < 0: 
             self.alive = False
 
-    def check_colliding(self, dinos):
-        for dino in dinos:
+    def check_colliding(self, dinos, genomes, networks):
+        for i, dino in enumerate(dinos):
             if self.rect.colliderect(dino.rect):
 
                 offset_x = self.rect.x - dino.rect.x
                 offset_y = self.rect.y - dino.rect.y
                 
                 if dino.mask.overlap(pygame.mask.from_surface(self.image), (offset_x, offset_y)):
-                    dinos.remove(dino)
+                    genomes[i].fitness -= 5
+                    genomes.pop(i)
+                    networks.pop(i)
+                    dinos.remove(dino) 
  
-
-
     def draw(self, surface):
         surface.blit(self.image, self.rect)
 
 class Game:
-    def __init__(self):
+    def __init__(self, genomes, config):
+        self.networks = []
+        self.genomes = []
+        self.birds = []
+        self.config = config
+        self.config_genomes = genomes
+
         self.running = True
         self.gamespeed = 8
         self.score = 0
-        self.dinos = [Dino()]
+        self.dinos = []
         self.ground = Ground() 
         self.obstacles = []
         self.obstacle_spawn_ticker = 0
         self.min_score_birds_start_spawning = 600
         self.obstacle_index = 0
+    
+    def create_neural_networks(self):
+        for genome_id, genome in self.config_genomes: 
+            genome.fitness = 0  
+
+            network = neat.nn.FeedForwardNetwork.create(genome, self.config)
+            self.networks.append(network)
+            self.dinos.append(Dino())
+            self.genomes.append(genome)
 
     def handle_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
                 self.running = False
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
-                    self.dinos[0].jump()
-                    print(self.obstacles[self.obstacle_index].rect.x - self.dinos[0].rect.x)
-                if event.key == pygame.K_DOWN:
-                    self.dinos[0].duck()
-            if event.type == pygame.KEYUP:
-                if event.key == pygame.K_DOWN:
-                    self.dinos[0].is_ducking = False
+        
+        for i, dino in enumerate(self.dinos):
+            if self.obstacles:
+                next_obstacle = self.obstacles[self.obstacle_index]
+
+                output = self.networks[i].activate(
+                    (next_obstacle.rect.x - dino.rect.x, next_obstacle.rect.y, self.gamespeed, 1 if dino.is_jumping else 0, 1 if dino.is_ducking else 0, next_obstacle.image.get_width(), next_obstacle.image.get_height())
+                ) # Input 1: distance between dino and next obstacle. Input 2: next obstacle's y. Input 3: gamespeed. Input 4: is dino currently jumping?. Input 5: is dino currently ducking?. 
+                # Input 6: next obstacle width. Input 7: next obstacle height.
+
+                if output[0] > 0.5: # Tell the dino he is a good boy if he jumps close to a cactus, tell dino he is a bad boy if not!
+                    dino.jump()
+                    distance = next_obstacle.rect.x - dino.rect.x
+
+                    if distance > 200:
+                        self.genomes[i].fitness -= distance / 100 + 1
+                    else:
+                        self.genomes[i].fitness += 15
+
+                if output[1] > 0.5:  # Tell the dino he is a good boy if he ducks when a bird is the next obstacle, tell dino he is a bad boy if a cactus is the next obstacle
+                    if isinstance(next_obstacle, Bird):  
+                        self.genomes[i].fitness += 5  
+                    elif isinstance(next_obstacle, Cactus):  
+                        self.genomes[i].fitness -= 2
 
     def spawn_cactus(self):
 
@@ -271,46 +313,48 @@ class Game:
         self.obstacles.append(new_bird)
     
     def spawn_obstacles(self):
-        if self.obstacle_spawn_ticker > random.randint(50, 200):
-            if not self.score < self.min_score_birds_start_spawning:
-                if random.randint(1,100) <= 75:
+        if self.obstacle_spawn_ticker >= random.randint(50, 200):
+            # Check if score is high enough to spawn birds
+            if self.score >= self.min_score_birds_start_spawning:
+                if random.randint(1, 100) <= 75:
                     self.spawn_cactus()
-                    self.obstacle_spawn_ticker = 0
                 else:
                     self.spawn_bird()
-                    self.obstacle_spawn_ticker = 0
-            else:
-                if random.randint(1,100) <= 80:
+            else:  # Spawn only cactus if the score is low
+                if random.randint(1, 100) <= 80:
                     self.spawn_cactus()
-                    self.obstacle_spawn_ticker = 0
-                else:
-                    return
+            
+            # Reset the ticker after spawning
+            self.obstacle_spawn_ticker = 0
 
     def update(self):
         if not self.dinos:
             self.running = False
 
         for dino in self.dinos:
-            dino.update()   
+            dino.update()
 
         if self.obstacles:
-            for obstacle in self.obstacles:
-                if (len(self.obstacles) > 1 and self.obstacles[0].passed):
-                    self.obstacle_index = 1
+            for i, obstacle in enumerate(self.obstacles):
+                if not obstacle.passed:
+                    self.obstacle_index = i
                 else:
                     self.obstacle_index = 0
 
-                if not obstacle.alive:  
-                    self.obstacles.remove(obstacle)
+                if not obstacle.alive:
+                    self.obstacles.pop(i)
                     continue
 
-                obstacle.update(self.gamespeed)
-                obstacle.check_colliding(self.dinos)
+                obstacle.update(self.gamespeed, self.genomes)
+                obstacle.check_colliding(self.dinos, self.genomes, self.networks)
 
 
         self.ground.update(self.gamespeed)
         
         self.spawn_obstacles()
+
+        for genome in self.genomes:
+            genome.fitness += 0.05
 
         self.score += 0.1
         self.gamespeed += 0.0015
@@ -320,27 +364,56 @@ class Game:
         screen.fill((255, 255, 255))  # White background
         self.ground.draw(screen)
 
-        for dino in self.dinos:
-            dino.draw(screen)
-
         for obstacle in self.obstacles:
             obstacle.draw(screen)
 
-        # Display score
+        for dino in self.dinos:
+            dino.draw(screen)
+
         font = pygame.font.Font(None, 36)
+
         score_text = font.render(f"Score: {round(self.score)}", True, (0, 0, 0))
         screen.blit(score_text, (screen.get_width() - score_text.get_width() - 30, 10))
 
+        generation_text = font.render(f"Alive: {len(self.dinos)}", True, (0, 0, 0))
+        screen.blit(generation_text, (10, 10))
+
+        generation_text = font.render(f"Gen: {generation}", True, (0, 0, 0))
+        screen.blit(generation_text, (10, 40))
+
         pygame.display.flip()
 
-    def run(self):
+    def run(self, generation):
+        self.create_neural_networks()
         while self.running:
             self.handle_events()
             self.update()
             self.render()
             clock.tick(FPS)
 
-# Start the game
+# ------------- A.I stuff -------------
+
+def eval_genomes(genomes, config):
+    global generation
+    generation += 1
+
+    game = Game(genomes, config) 
+    game.run(generation)  
+
+
+def run_neat(config_path):
+    config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                         neat.DefaultSpeciesSet, neat.DefaultStagnation,
+                         config_path)
+
+    p = neat.Population(config)
+
+
+    winner = p.run(eval_genomes, 1000)
+
+    print('\nBest genome:\n{!s}'.format(winner))
+
 if __name__ == "__main__":
-    game = Game()
-    game.run()
+    local_dir = os.path.dirname((__file__))
+    config_path = os.path.join(local_dir, "neat-config.txt")
+    run_neat(config_path)
