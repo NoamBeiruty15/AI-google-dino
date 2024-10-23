f11f8b11be7906fd5226b3f4592c67d6061da3ba
Noam beiruty <noambeiruty@noams-mbp.home>
2024-10-23 13:03:22 +0300

    basic dino game, no A.I. yet

--- google_dino.py
+++ google_dino.py
@@ -271,7 +271,7 @@ class Game:
         self.obstacles.append(new_bird)
     
     def spawn_obstacles(self):
-        if self.obstacle_spawn_ticker > random.randint(50, 200):
+        if self.obstacle_spawn_ticker > random.randint(45, 200):
             if not self.score < self.min_score_birds_start_spawning:
                 if random.randint(1,100) <= 75:
                     self.spawn_cactus()
